1d86ef310a4c0d26f420fe520bf1e556cf0b383f
ivan <ivanmuratov.2016@gmail.com>
2023-10-25 15:52:50 +0300

    Homepage template created

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bedding_store/', include('bedding_store.urls')),
+    path('', include('bedding_store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
