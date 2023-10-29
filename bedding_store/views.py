990fde0deaa354aa79892e7003b240b451c4c7bb
ivan <ivanmuratov.2016@gmail.com>
2023-10-29 20:21:27 +0200

    login logout features added

--- bedding_store/views.py
+++ bedding_store/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
 from .models import Product
 
 
@@ -9,3 +11,25 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Ви успішно увійшли до облікового запису!')
+            return redirect('home')
+        else:
+            messages.success(request, 'Помилка авторизації! Спробуйте ще раз!')
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'Ви вийшли з облікового запису! Дякуємо за увагу до нашої крамниці!')
+    return redirect('home')
