a0501a0f5acb3e52c351244f154d1782339dc35f
ivan <ivanmuratov.2016@gmail.com>
2024-07-31 21:57:00 +0300

    payment views and checkout.html fixed

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from cart.cart import Cart
+
 def checkout(request):
-	return render(request, "payment/checkout.html", {})
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quantity
+    totals = cart.cart_total()
+    return render(request, "payment/checkout.html", {'cart_products': cart_products, 'quantities': quantities, 'totals': totals})
 
 
 def payment_success(request):
