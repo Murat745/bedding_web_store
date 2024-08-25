09ab01284d13d7e599379e706f861220063a95b4
ivan <ivanmuratov.2016@gmail.com>
2024-08-25 14:36:36 +0300

    billing_info, billing form added

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from cart.cart import Cart
-
-from payment.forms import ShippingForm
+from django.contrib import messages
+from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress
+
+
+def billing_info(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quantity
+        totals = cart.cart_total()
+
+        # Check to see if user is logged in
+        if request.user.is_authenticated:
+            billing_form = PaymentForm()
+            return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_inf": request.POST, "billing_form": billing_form})
+        
+        else:
+            billing_form = PaymentForm()
+            return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_inf": request.POST, "billing_form": billing_form})
+
+        
+    else:
+         messages.success(request, 'Зареєструйтесь або увійдіть у Ваш обліковий запис')
+         return redirect('home')
+         
+
 
 def checkout(request):
     cart = Cart(request)
