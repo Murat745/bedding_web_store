b050cb1f24d33f811a6a9afa66be84285e200b87
ivan <ivanmuratov.2016@gmail.com>
2024-09-26 15:46:04 +0300

    railway fixes

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DJANGO_SECRET_KEY')
 
 ALLOWED_HOSTS = ['*'] #['beddingwebstore-production.up.railway.app', 'https://beddingwebstore-production.up.railway.app']
 #CSRF_TRUSTED_ORIGINS = ['https://beddingwebstore-production.up.railway.app']
