74f61a4df12d219757ec30514120574cd28e6a26
ivan <ivanmuratov.2016@gmail.com>
2024-09-28 21:12:20 +0300

    railway changes1

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_SECRET_KEY')
 
-ALLOWED_HOSTS = ['beddingwebstore-production.up.railway.app', 'https://beddingwebstore-production.up.railway.app', 'localhost']
+ALLOWED_HOSTS = ['beddingwebstore-production.up.railway.app', 'https://beddingwebstore-production.up.railway.app', 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://beddingwebstore-production.up.railway.app']
 
 
